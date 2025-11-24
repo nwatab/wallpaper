46fcaa0d845c01568ea0bd63aa782ecd7ccf0cea
Naoki Watanabe <asterisk37n@gmail.com>
2025-11-25 00:30:51 +0900

    remove basepath

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  output: "export",
-  basePath: "/wallpaper",
+  output: 'export',
   images: {
     unoptimized: true,
   },
